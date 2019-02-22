1383b17a6e3ad1cb96e8df5873b22f9f6e2e595d
Styopa Semenukha <ssemenukha@developmentgateway.org>
2019-02-22 17:02:33 -0500

    Allow relaying for private subnets

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,13 @@
 #!/bin/sh -e
 : ${OPENSMTPD_LISTEN_ON:=0.0.0.0;::}
+: ${OPENSMTPD_RULES:=accept from source <private> for any relay}
 if [ "$1" = "opensmtpd" ]; then
   (
     IFS=';'
     for IFACE in $OPENSMTPD_LISTEN_ON; do
       echo "listen on $IFACE"
     done
+    echo 'table private { 172.16.0.0/12, 192.168.0.0/16, 10.0.0.0/8, fd00::/8 }'
     echo "$OPENSMTPD_RULES" | sed 's/;[[:space:]]*/\n/g'
   ) >/etc/smtpd/smtpd.conf
   smtpd -n

ae0d4158ca4a35dc995405e6b89d5c213bd3e3c0
Styopa Semenukha <ssemenukha@developmentgateway.org>
2019-02-25 16:53:26 -0500

    Trust loopback addresses

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,12 @@
 #!/bin/sh -e
 # Copyright 2019, Development Gateway, see COPYING
-: ${OPENSMTPD_RULES:=accept from source <private> for any relay}
+: ${OPENSMTPD_RULES:=accept from source <trusted> for any relay}
 
 cat >/etc/smtpd/smtpd.conf <<EOF
 listen on 0.0.0.0
 listen on ::
 
-table private { 172.16.0.0/12, 192.168.0.0/16, 10.0.0.0/8, fd00::/8 }
+table trusted { 172.16.0.0/12, 192.168.0.0/16, 10.0.0.0/8, fd00::/8, 127.0.0.0/8, ::1/128 }
 
 $(echo "$OPENSMTPD_RULES" | sed 's/;[[:space:]]*/\n/g')
 EOF

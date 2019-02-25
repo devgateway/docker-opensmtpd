fee396890c9a3a0e2c5b78444d3a147d90906ed0
Styopa Semenukha <ssemenukha@developmentgateway.org>
2019-02-25 15:27:34 -0500

    Simplify configuration

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,16 @@
 #!/bin/sh -e
 # Copyright 2019, Development Gateway, see COPYING
-: ${OPENSMTPD_LISTEN_ON:=0.0.0.0;::}
 : ${OPENSMTPD_RULES:=accept from source <private> for any relay}
-(
-  IFS=';'
-  for IFACE in $OPENSMTPD_LISTEN_ON; do
-    echo "listen on $IFACE"
-  done
-  echo 'table private { 172.16.0.0/12, 192.168.0.0/16, 10.0.0.0/8, fd00::/8 }'
-  echo "$OPENSMTPD_RULES" | sed 's/;[[:space:]]*/\n/g'
-) >/etc/smtpd/smtpd.conf
+
+cat >/etc/smtpd/smtpd.conf <<EOF
+listen on 0.0.0.0
+listen on ::
+
+table private { 172.16.0.0/12, 192.168.0.0/16, 10.0.0.0/8, fd00::/8 }
+
+$(echo "$OPENSMTPD_RULES" | sed 's/;[[:space:]]*/\n/g')
+EOF
+
 smtpd -n
+
 exec smtpd -dP mda
